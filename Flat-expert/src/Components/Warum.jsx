57ec3791ea6204fd7bd0c7d1dc9ace6781cb0adf
Mirza Abdullah Baig <abdullah.baig2013@gmail.com>
2024-02-11 20:23:13 +0100

    Warum mit uns done

--- Flat-expert/src/Components/Warum.jsx
+++ Flat-expert/src/Components/Warum.jsx
@@ -1,74 +1,66 @@
 import React from "react";
-import backgroundImage from "../assets/warum.png"; // Import your background image here
+import backgroundImage from "../assets/warum.png";
 
 const Warum = () => {
   return (
-    <div
-      className="p-4 bg-cover bg-no-repeat bg-center md:bg-cover md:bg-no-repeat"
-      style={{ backgroundImage: `url(${backgroundImage})` }}
-    >
-      <h1 className="text-5xl font-bold mb-1 mt-10 md:mt-20 ultra text-white  ">
-        WARUM MIT UNS ?
-      </h1>
-      <div className="border-b-2 border-gray-300 mb-4 ml-7 mr-10 md:ml-0 md:mr-0"></div>
-      <div className="flex justify-center items-center md:h-screen">
-        <div className="grid  lg:grid-cols-4 gap-4">
-          <div className="max-w-xs p-6 border border-white rounded-lg hover:bg-gray-700 dark:bg-gray-800 md:h-auto">
-            <div className="bg-opacity-50 h-full">
-              <h5 className="mb-2 text-lg md:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                INDIVIDUELLE BETREUUNG
-              </h5>
-              <p className="font-normal text-sm md:text-base text-gray-700 dark:text-gray-400 mt-3 md:mt-7">
-                Wir bieten eine persönliche und maßgeschneiderte Beratung, die
-                sich auf die individuellen Bedürfnisse jedes Kunden
-                konzentriert.
-              </p>
-            </div>
+    <div className="flex justify-center">
+      <div className="max-w-md mx-2 my-20 bg-black bg-opacity-70 border-white border-2 overflow-hidden shadow-md rounded-lg">
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl text-white mb-2">
+            STANDORTANALYSE
           </div>
+          <p className="text-white text-base">
+            Ermittlung des idealen Wohnortes basierend auf individuellen
+            Kundenpräferenzen, wie Verkehrsanbindung und Freizeitmöglichkeiten.
+          </p>
+        </div>
+      </div>
 
-          <div className="max-w-xs p-6 border border-white rounded-lg hover:bg-gray-700 dark:bg-gray-800 md:h-auto">
-            <div className="bg-opacity-50 h-full">
-              <h5 className="mb-2 text-lg md:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                UMFASSNDER SERVICE
-              </h5>
-              <p className="font-normal text-sm md:text-base text-gray-700 dark:text-gray-400 mt-3 md:mt-7">
-                Unser breites Servicepaket deckt alle Aspekte der Wohnungssuche
-                ab – von der Standortanalyse bis zur Überprüfung des
-                Mietvertrags.
-              </p>
-            </div>
-          </div>
+      <div className="max-w-md mx-2 my-20 bg-black bg-opacity-70 border-white border-2 overflow-hidden shadow-md rounded-lg">
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl text-white mb-2">WOHNUNGSSUCHE</div>
+          <p className="text-white text-base">
+            Zusammenarbeit mit Hausverwaltungen und Wohnungsbaugesellschaften
+            zur gezielten Suche passender Wohnungen unter optimalen Bedingungen.
+          </p>
+        </div>
+      </div>
 
-          <div className="max-w-xs p-6 border border-white rounded-lg hover:bg-gray-700 dark:bg-gray-800 md:h-auto">
-            <div className="bg-opacity-50 h-full">
-              <h5 className="mb-2 text-lg md:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                PREMIUM ANGEBOTE
-              </h5>
-              <p className="font-normal text-sm md:text-base text-gray-700 dark:text-gray-400 mt-3 md:mt-7">
-                Durch exklusive Accounts auf Premium-Wohnungsportalen
-                ermöglichen wir unseren Kunden den Zugang zu hochwertigen
-                Wohnungsangeboten.
-              </p>
-            </div>
-          </div>
-
-          <div className="max-w-xs p-6 border border-white rounded-lg hover:bg-gray-700 dark:bg-gray-800 md:h-auto">
-            <div className="bg-opacity-50 h-full">
-              <h5 className="mb-2 text-lg md:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                SICHERHEIT
-              </h5>
-              <p className="font-normal text-sm md:text-base text-gray-700 dark:text-gray-400 mt-3 md:mt-7">
-                Wir legen großen Wert auf die Sicherheit unserer Kunden und
-                bieten aktive Beratung zur Betrugsprävention während des
-                gesamten Wohnungsfindungsprozesses
-              </p>
-            </div>
-          </div>
-          {/* End of repeating div */}
+      <div className="max-w-md mx-2 my-20 bg-black bg-opacity-70 border-white border-2 overflow-hidden shadow-md rounded-lg">
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl text-white mb-2">BERATUNG</div>
+          <p className="text-white text-base">
+            Persönliche Sitzungen zur Klärung individueller Anforderungen und
+            maßgeschneiderter Unterstützung bei der Wohnungssuche.
+          </p>
         </div>
       </div>
     </div>
   );
 };
 
-export default Warum;
+const WarumPage = () => {
+  return (
+    <div
+      className="relative"
+      style={{
+        height: "700px", // You can adjust the height here
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="absolute top-8 p-8 lg:left-20">
+        <div className="bg-black bg-opacity-60 rounded-lg">
+          <h1 className="text-5xl font-bold mb-20 text-white ultra">
+            WARUM MIT <br /> UNS?
+          </h1>
+        </div>
+      </div>
+      <div className="flex justify-center items-center h-full">
+        <Warum />
+      </div>
+    </div>
+  );
+};
+
+export default WarumPage;
